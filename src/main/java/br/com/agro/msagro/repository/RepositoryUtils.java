5f90e774d61f6a529c87eb7285254cabe02dc95f
Márcio Santos <marcioads2013@gmail.com>
2023-03-13 16:21:05 -0300

    Update RepositoryUtils.java
    ajuste consulta paginada

--- src/main/java/br/com/agro/msagro/repository/RepositoryUtils.java
+++ src/main/java/br/com/agro/msagro/repository/RepositoryUtils.java
@@ -18,33 +18,57 @@ public class RepositoryUtils {
 
     public List<Predicate> aplicarFiltro(Root<?> rootEntidade, Map<String, Object> filtros) {
         List<Predicate> listaPredicate = new ArrayList<Predicate>();
-
+        if(filtros == null) return Collections.emptyList();
         for (final String chave : filtros.keySet()) {
+            Object value = filtros.get(chave);
+            if(value == null || UtilString.isEmpty(value.toString())) continue;
             if (UtilObjeto.isNotNull(filtros.get(chave)) && !(filtros.get(chave).toString().isEmpty())) {
-                String atributo = chave.replace(".", "#");
-                String[] campos = atributo.split("#");
-                Path path = null;
-                if(rootEntidade.get(campos[0]).getJavaType().isAssignableFrom(Collection.class)){
-                    Join joinAnother = rootEntidade.join(campos[0], JoinType.INNER);
-                    path = joinAnother.get(campos[1]);
-                }else{
-                    path = rootEntidade.get(campos[0]);
-                    for(int i = 1; (i + 1) <= campos.length; i++){
-                        path = path.get(campos[i]);
+                if(chave.contains(".")){
+                    String atributo = chave.replace(".", "#");
+                    String[] campos = atributo.split("#");
+                    Path path = null;
+                    if(rootEntidade.get(campos[0]).getJavaType().isAssignableFrom(Collection.class)){
+                        Join joinAnother = rootEntidade.join(campos[0], JoinType.INNER);
+                        path = joinAnother.get(campos[1]);
+                    }else{
+                        path = rootEntidade.get(campos[0]);
+                        for(int i = 1; (i + 1) <= campos.length; i++){
+                            path = path.get(campos[i]);
+                        }
                     }
-                }
 
-                Object value = filtros.get(chave);
-                if(value == null || UtilString.isEmpty(value.toString())) continue;
                     if(path.getJavaType().isEnum()){
                         listaPredicate.add(getCriteriaBuilder().equal(path.as(String.class), value));
 
                     }else if (value instanceof String) {
                         listaPredicate.add(getCriteriaBuilder().like(getCriteriaBuilder().upper(path), "%" + value.toString().toUpperCase() + "%"));
 
-                } else {
-                    listaPredicate.add(getCriteriaBuilder().or(getCriteriaBuilder().equal(path, value), getCriteriaBuilder().isNull(path)));
+                    } else {
+                        listaPredicate.add(getCriteriaBuilder().or(getCriteriaBuilder().equal(path, value), getCriteriaBuilder().isNull(path)));
+                    }
+                }else if(chave.contains(":")){
+                    String[] campos = chave.split(":");
+                    List<Predicate> predsOr = new ArrayList<>();
+                    for(String str : campos){
+                        if(UtilString.isNumerico(value.toString())){
+                            predsOr.add(getCriteriaBuilder().equal(rootEntidade.get(str), value));
+                        }else{
+                            predsOr.add(getCriteriaBuilder().like(getCriteriaBuilder().upper(rootEntidade.get(str)), "%" + value.toString().toUpperCase() + "%"));
+                        }
+                    }
+                    listaPredicate.add(getCriteriaBuilder().or(predsOr.toArray(new Predicate[0])));
+                }else{
+                    if(rootEntidade.get(chave).getJavaType().isEnum()){
+                        listaPredicate.add(getCriteriaBuilder().equal(rootEntidade.get(chave).as(String.class), value));
+
+                    }else if (value instanceof String) {
+                        listaPredicate.add(getCriteriaBuilder().like(getCriteriaBuilder().upper(rootEntidade.get(chave)), "%" + value.toString().toUpperCase() + "%"));
+
+                    } else {
+                        listaPredicate.add(getCriteriaBuilder().or(getCriteriaBuilder().equal(rootEntidade.get(chave), value), getCriteriaBuilder().isNull(rootEntidade.get(chave))));
+                    }
                 }
+
             }
         }
 
